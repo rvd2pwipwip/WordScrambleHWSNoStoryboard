ffda8eec93dc0fc657bedae1e54e192975aac47b
Herve Desrosiers <hdesrosiers@stingray.com>
2020-02-18 17:53:14 -0500

    finished part 6, user feedback, ready for wrap-up challenge

--- WordScrambleHWSNoStoryboard/ViewController.swift
+++ WordScrambleHWSNoStoryboard/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UITableViewController {
     
     var allWords = [String]()
     var usedWords = [String]() // 1. define an array containing the data to display in tableView
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,7 +25,7 @@ class ViewController: UITableViewController {
                 allWords = startWords.components(separatedBy: "\n")
             }
         }
-
+        
         if allWords.isEmpty {
             allWords = ["silkworm"]
         }
@@ -42,34 +42,52 @@ class ViewController: UITableViewController {
     @objc func promptForAnswer() {
         let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
         ac.addTextField()
-
+        
         let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
             guard let answer = ac?.textFields?[0].text else { return }
             self?.submit(answer)
         }
-
+        
         ac.addAction(submitAction)
         present(ac, animated: true)
     }
     
     func submit(_ answer: String) {
         let lowerAnswer = answer.lowercased()
-
+        let errorTitle: String
+        let errorMessage: String
+        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
                     usedWords.insert(answer, at: 0)
-
+                    
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            } else {
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
             }
+        } else {
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title)"
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func isPossible(word: String) -> Bool {
         guard var tempWord = title?.lowercased() else { return false }
-
+        
         for letter in word {
             if let position = tempWord.firstIndex(of: letter) {
                 tempWord.remove(at: position)
@@ -77,22 +95,22 @@ class ViewController: UITableViewController {
                 return false
             }
         }
-
+        
         return true
     }
-
+    
     func isOriginal(word: String) -> Bool {
         return !usedWords.contains(word)
     }
-
+    
     func isReal(word: String) -> Bool {
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-
+        
         return misspelledRange.location == NSNotFound
     }
-
+    
 }
 
 // 2.
@@ -108,7 +126,7 @@ extension ViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         // Configure the cell’s contents.
         cell.textLabel?.text = usedWords[indexPath.row] // also reassign cell content when cells are reused
-
+        
         return cell
     }
 }
